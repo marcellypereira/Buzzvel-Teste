69fb70db51282a663fa1d633295a1824723ec4f1
marcellypereira <marcellyp120@gmail.com>
2024-10-26 12:41:57 -0300

    :sparkles: feat: Creating the Header and Button components

---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import OutlineButton from "./Button";
+
+export default function Header() {
+  return (
+    <header className="absolute top-0 left-0 w-full z-50">
+      <nav
+        className="max-w-[1920px] mx-auto p-4 flex justify-between items-center"
+        aria-label="Main Navigation"
+      >
+        <div className="flex items-center">
+          <h1 className="text-black text-3xl font-bold mr-8">
+            <a href="#" aria-label="">
+              soller
+            </a>
+          </h1>
+          <ul className="flex items-center gap-x-8">
+            <li className="hidden lg:block">
+              <a href="#" className="text-[#0F172A] text-base font-medium hover:text-[#581C87]">
+                Products
+              </a>
+            </li>
+            <li className="hidden lg:block">
+              <a href="#" className="text-[#0F172A] text-base font-medium hover:text-[#581C87]">
+                Solutions
+              </a>
+            </li>
+            <li className="hidden lg:block">
+              <a href="#" className="text-[#0F172A] text-base font-medium hover:text-[#581C87]">
+                Services
+              </a>
+            </li>
+            <li className="hidden lg:block">
+              <a href="#" className="text-[#0F172A] text-base font-medium hover:text-[#581C87]">
+                Configure
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="hidden lg:flex items-center gap-4">
+          <div
+            className="flex items-center gap-3"
+            aria-label="Contact Information"
+          >
+            <img src="Vector.png" alt="Contact icon" />
+            <p className="text-base text-[#0369A1]">55 818 282</p>
+          </div>
+          <OutlineButton text="Request a Quote" variant="primary" />
+        </div>
+      </nav>
+    </header>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#581C87",
+        dark: "#0F172A",
+        blue: "#0369A1",
+        blueLight: "#BAE6FD",
+        light: "#FFFFFF",
+        gray: "#475569",
+        grayLight:"#e7e7e7",
+        orange: "#D97706",
+        yellow: "#FCD34D",
+        brown: "#78350F",
+        puerple: "#7E22CE",
+      }
+    },
   },
   plugins: [],
 }
