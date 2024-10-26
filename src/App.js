4c7532824d2ae3930e8fac52828376ae7855dffd
marcellypereira <marcellyp120@gmail.com>
2024-10-26 13:12:48 -0300

    :sparkles: feat:Create Solutions section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Hero from './sections/Hero';
 import Products from './sections/Products.js';
 import Services from './sections/Services/index.js';
+import Solutions from './sections/Solutions/index.js';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Products />
       <Services />
+      <Solutions />
     </>
   );
 }
