2b508894cf6e6629fa56a66e85b6781e485ecdaf
marcellypereira <marcellyp120@gmail.com>
2024-10-29 18:58:19 -0300

    :recycle: refactor: Code refactoring and sitemap creation

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const Card = ({ text, image, name, power }) => {
   return (
     <article className="flex-shrink-0 p-4 card">
       <div className="bg-[#FFFFFF] shadow-md rounded-lg p-8 lg:max-w-[364px] lg:h-[442px] md:max-w-[364px] md:h-[442px] max-w-[358px] h-[359px] flex flex-col justify-center transition-transform duration-300 hover:scale-105">
-        <p className="lg:text-lg md:text-lg text-base font-normal lg:leading-[28.8px] md:leading-[28.8px] leading-[22.4px] mb-4 pt-24 lg:max-w-[300px]md:max-w-[300px] max-w-[294px] text-[#0F172A]">{text}</p>
+        <p className="lg:text-lg md:text-lg text-base font-normal lg:leading-[28.8px] md:leading-[28.8px] leading-[22.4px] mb-4 pt-24 lg:max-w-[300px]md:max-w-[300px] max-w-[294px] text-[#0F172A]">
+          {text}
+        </p>
         <div className="flex items-center mt-4">
-          <LazyLoadImage src={image} alt={`Imagem de ${name}`} className="mr-4" />
+          <LazyLoadImage
+            src={image}
+            alt={`Imagem de ${name}`}
+            className="mr-4"
+          />
           <div>
-            <h2 className="lg:text-lg md:text-lg text-base	font-normal lg:leading-[28.8px] md:leading-[28.8px] leading-[22.4px] text-[#0F172A] lg:max-w-[220px]md:max-w-[220px] max-w-[238px]">{name}</h2>
-            <p className="text-[#475569] lg:text-base md:text-base text-sm lg:leading-[22.4px] md:leading-[22.4px] leading-[19.6px]">{power}</p>
+            <h2 className="lg:text-lg md:text-lg text-base	font-normal lg:leading-[28.8px] md:leading-[28.8px] leading-[22.4px] text-[#0F172A] lg:max-w-[220px]md:max-w-[220px] max-w-[238px]">
+              {name}
+            </h2>
+            <p className="text-[#475569] lg:text-base md:text-base text-sm lg:leading-[22.4px] md:leading-[22.4px] leading-[19.6px]">
+              {power}
+            </p>
           </div>
         </div>
       </div>
--- src/Components/Text.js
+++ src/Components/Text.js
@@ -1,48 +1,58 @@
-import React from 'react';
+import React from "react";
 
-export const Title = ({ text, className = '' }) => {
+export const Title = ({ text, className = "" }) => {
   return (
-    <h1 className={`text-[32px] leading-[35.2px] font-bold mt-4 mb-6 
+    <h1
+      className={`text-[32px] leading-[35.2px] font-bold mt-4 mb-6 
       md:text-[32px] md:leading-[40px] md:font-extrabold
-      lg:text-[56px] lg:leading-[61.6px] lg:font-extrabold ${className}`}>
+      lg:text-[56px] lg:leading-[61.6px] lg:font-extrabold ${className}`}
+    >
       {text}
     </h1>
   );
 };
 
-export const Description = ({ text, className = '' }) => {
+export const Description = ({ text, className = "" }) => {
   return (
-    <p className={`text-base leading-[22.4px] font-normal md:text-xl md:leading-9 md:font-normal lg:text-xl lg:leading-9 lg:font-normal ${className}`}>
+    <p
+      className={`text-base leading-[22.4px] font-normal md:text-xl md:leading-9 md:font-normal lg:text-xl lg:leading-9 lg:font-normal ${className}`}
+    >
       {text}
     </p>
   );
 };
 
-export const SubTitle = ({ text, className = '' }) => {
+export const SubTitle = ({ text, className = "" }) => {
   return (
-    <p className={`text-base leading-[17.6px] font-medium mt-16 
+    <p
+      className={`text-base leading-[17.6px] font-medium mt-16 
       md:text-xl md:leading-[22px] md:font-medium 
-      lg:text-xl lg:leading-[22px] lg:font-medium ${className}`}>
+      lg:text-xl lg:leading-[22px] lg:font-medium ${className}`}
+    >
       {text}
     </p>
   );
 };
 
-export const H2 = ({ text, className = '' }) => {
+export const H2 = ({ text, className = "" }) => {
   return (
-    <h2 className={`text-xl leading-[22px] font-medium max-w-[147.5px]
+    <h2
+      className={`text-xl leading-[22px] font-medium max-w-[147.5px]
       md:text-2xl font-bold leading-[26.4px] md:max-w-[566px] 
-      lg:text-2xl font-bold leading-[26.4px] lg:max-w-[566px] ${className}`}>
+      lg:text-2xl font-bold leading-[26.4px] lg:max-w-[566px] ${className}`}
+    >
       {text}
     </h2>
   );
 };
 
-export const P = ({ text, className = '' }) => {
+export const P = ({ text, className = "" }) => {
   return (
-    <p className={`text-base leading-[22.4px] font-normal max-w-[147.5px] text-center 
+    <p
+      className={`text-base leading-[22.4px] font-normal max-w-[147.5px] text-center 
       md:text-lg font-normal leading-[28.8px] md:max-w-[566px] md:text-left 
-      lg:text-lg font-normal leading-[28.8px] lg:max-w-[566px] lg:text-left mt-4 ${className}`}>
+      lg:text-lg font-normal leading-[28.8px] lg:max-w-[566px] lg:text-left mt-4 ${className}`}
+    >
       {text}
     </p>
   );
